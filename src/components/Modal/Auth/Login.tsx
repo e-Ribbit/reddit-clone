59904f14dd42f9ec2b523e031ce23110e59bd191
e-Ribbit <jstecode@gmail.com>
2023-02-15 19:25:26 +0100

    styled login

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Input } from "@chakra-ui/react";
+import { Button, Input } from "@chakra-ui/react";
 
 type LoginProps = {};
 
@@ -8,21 +8,57 @@ const Login: React.FC<LoginProps> = () => {
     email: "",
     password: "",
   });
+
+  const onSubmit = () => {};
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // update form state
+    setLoginForm((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <Input
+        required
         name="email"
         placeholder="email"
         type="email"
         mb={2}
-        onChange={() => {}}
+        onChange={onChange}
+        fontSize="10pt"
+        _placeholder={{ color: "grey.500" }}
+        _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        bg="gray.50"
       />
       <Input
+        required
         name="password"
         placeholder="password"
         type="password"
-        onChange={() => {}}
+        onChange={onChange}
+        mb={2}
+        fontSize="10pt"
+        _placeholder={{ color: "grey.500" }}
+        _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        bg="gray.50"
       />
+      <Button type="submit" width="100%" height="36px" mt={2} mb={2}>
+        Login
+      </Button>
     </form>
   );
 };
